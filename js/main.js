023ee69a84d1bab46d132c1f9a04b43feba23ed2
Nidhoggui <guilhermemelquiades42@gmail.com>
2020-06-14 21:46:37 -0300

    conectando ao banco

--- js/main.js
+++ js/main.js
@@ -3,15 +3,43 @@ db.transaction(function(criar){
   criar.executeSql("CREATE TABLE granjas (ID INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, cnpj TEXT)");
 });
 
+const addButton = document.querySelector('.addButton');
+var iputValue = document.querySelector('.input');
+const countainer = document.querySelector('.countainer');
+
+class granja{
+  counstructor(nome, email, cnpj){
+    this.createDiv(nome);
+    this.createDiv(email);
+    this.createDiv(cnpj);
+  }
+
+  createDiv(nome){
+    let input = document.createElement('input');
+    input.value = nome;
+    input.disabled = true;
+    input.classList.add('')
+  }
+}
 function cadastrar(){
   var nome = document.getElementById("nome").value;
-  var email = document.getElementById("email").value;
+  var email = document.getElementById("email").value + '@email.com';
   var cnpj = document.getElementById("cnpj").value;
 
   db.transaction(function(armazenar){
-    armazenar.executeSql("INSERT INTO granjas (nome, email, cnpj) VALUES (?,?@email.com,?)", [nome, email, cnpj]);
+    armazenar
+       .executeSql(
+           "INSERT INTO granjas (nome, email, cnpj) VALUES (?,?,?)",
+           [nome, email, cnpj],
+           function() { 
+                console.log("SUCCESS");
+                alert("Granja " + document.getElementById("nome").value + " Cadastrada!");
+            },
+           function(data, error) {
+                console.log("There was an ERROR", error)
+                alert("There was a technical error. Try again.");
+            });
   });
-  alert("Granja " + document.getElementById("nome").value + " Cadastrada!");
 };
 
 function voltar(){
